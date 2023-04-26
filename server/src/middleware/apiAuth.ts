c5bea6e979af7b2fd2e4a747588a2efc05d7f324
Vlăduț Voicu <107242590+vladutvoicu@users.noreply.github.com>
2023-04-26 17:33:39 +0000

    small fix

--- server/src/middleware/apiAuth.ts
+++ server/src/middleware/apiAuth.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { config } from "../config/config";
 
 const apiAuth = (req: Request, res: Response, next: NextFunction) => {
-    const apiKey = req.headers["Authorization"];
+    const apiKey = req.headers.authorization;
     if (!apiKey) {
         return res.status(401).json({ message: "API key missing" });
     }
