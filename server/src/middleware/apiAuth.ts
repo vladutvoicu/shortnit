1a93b2dbc9a7dad93eb6dd7c284fa3d5cb6b9b9a
Vlăduț Voicu <107242590+vladutvoicu@users.noreply.github.com>
2023-04-26 17:15:24 +0000

    header fix

--- server/src/middleware/apiAuth.ts
+++ server/src/middleware/apiAuth.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { config } from "../config/config";
 
 const apiAuth = (req: Request, res: Response, next: NextFunction) => {
-    const apiKey = req.headers["api-key"];
+    const apiKey = req.headers["Authorization"];
     if (!apiKey) {
         return res.status(401).json({ message: "API key missing" });
     }
